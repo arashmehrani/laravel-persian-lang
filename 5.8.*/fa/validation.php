1f46fdba0718201bd85aed745b8361d0190354f8
Arash Mehrani <45669243+arashmehrani@users.noreply.github.com>
2019-07-27 00:26:29 +0430

    Update validation.php

--- 5.8.*/fa/validation.php
+++ 5.8.*/fa/validation.php
@@ -145,6 +145,38 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+        'name' => 'نام',
+        'username' => 'نام کاربری',
+        'email' => 'ایمیل',
+        'first_name' => 'نام',
+        'last_name' => 'نام خانوادگی',
+        'password' => 'رمز عبور',
+        'password_confirmation' => 'تاییدیه ی رمز عبور',
+        'city' => 'شهر',
+        'country' => 'کشور',
+        'address' => 'نشانی',
+        'phone' => 'تلفن',
+        'mobile' => 'تلفن همراه',
+        'age' => 'سن',
+        'sex' => 'جنسیت',
+        'gender' => 'جنسیت',
+        'day' => 'روز',
+        'month' => 'ماه',
+        'year' => 'سال',
+        'hour' => 'ساعت',
+        'minute' => 'دقیقه',
+        'second' => 'ثانیه',
+        'title' => 'عنوان',
+        'text' => 'متن',
+        'content' => 'محتوا',
+        'description' => 'توضیحات',
+        'excerpt' => 'گلچین کردن',
+        'date' => 'تاریخ',
+        'time' => 'زمان',
+        'available' => 'موجود',
+        'type' => 'نوع',
+        'size' => 'اندازه'
+    ],
 
 ];
